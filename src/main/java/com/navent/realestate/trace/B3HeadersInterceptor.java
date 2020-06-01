da11d468e0267a332e10ff3f059339dcfa77b2a1
Bernardo Falese <bfalese@navent.com>
2020-06-01 17:39:01 -0300

    PLTF-378 propagacion headers b3

---
+++ src/main/java/com/navent/realestate/trace/B3HeadersInterceptor.java
@@ -0,0 +1,30 @@
+package com.navent.realestate.trace;
+
+import org.slf4j.MDC;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class B3HeadersInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        for (int i = 0; i < B3Header.values().length; i++) {
+        	String b3Header = B3Header.values()[i].getName();
+        	Optional.of(request.getHeader(b3Header)).ifPresent(h -> MDC.put(b3Header, h));			
+		}
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+        for (int i = 0; i < B3Header.values().length; i++) {
+        	String b3Header = B3Header.values()[i].getName();
+        	MDC.remove(b3Header);
+		}
+    }
+}
---
+++ src/main/java/com/navent/realestate/trace/B3HeadersPropagator.java
@@ -0,0 +1,19 @@
+package com.navent.realestate.trace;
+
+import org.slf4j.MDC;
+
+import java.util.Optional;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class B3HeadersPropagator implements RequestInterceptor {
+
+	@Override
+	public void apply(RequestTemplate template) {
+		for (int i = 0; i < B3Header.values().length; i++) {
+        	String b3Header = B3Header.values()[i].getName();
+        	Optional.of(MDC.get(b3Header)).ifPresent(v -> template.header(b3Header, v));			
+		}
+	}
+}
