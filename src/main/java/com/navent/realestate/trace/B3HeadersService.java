7187920329ac4887e4e17b110ba7ddc85d953ee1
Bernardo Falese <bfalese@navent.com>
2020-06-02 10:58:11 -0300

    PLTF-378 mdc a mapa

--- src/main/java/com/navent/realestate/trace/B3HeadersService.java
+++ src/main/java/com/navent/realestate/trace/B3HeadersService.java
@@ -2,6 +2,8 @@ package com.navent.realestate.trace;
 
 import org.slf4j.spi.MDCAdapter;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +17,19 @@ public class B3HeadersService {
 		}
     }
 
-    public void cleanMDC(HttpServletRequest request, MDCAdapter mdc) {
+    public void cleanMDC(MDCAdapter mdc) {
         for (int i = 0; i < B3Header.values().length; i++) {
         	String b3Header = B3Header.values()[i].getName();
         	mdc.remove(b3Header);
 		}
+    }
+
+    public Map<String, String> mdcHeadersToMap(MDCAdapter mdc) {
+    	Map<String, String> headers = new HashMap<String, String>(B3Header.values().length);
+    	for (int i = 0; i < B3Header.values().length; i++) {
+        	String b3Header = B3Header.values()[i].getName();
+        	Optional.ofNullable(mdc.get(b3Header)).ifPresent(v -> headers.put(b3Header, v));
+    	}
+    	return headers;
     }
 }
