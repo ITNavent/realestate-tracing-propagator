174064484235915e0007f7bb3b1736fceda34f25
Bernardo Falese <bfalese@navent.com>
2020-06-03 12:38:06 -0300

    PLTF-378 log debug

--- src/main/java/com/navent/realestate/trace/B3HeadersService.java
+++ src/main/java/com/navent/realestate/trace/B3HeadersService.java
@@ -1,5 +1,7 @@
 package com.navent.realestate.trace;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.spi.MDCAdapter;
 
 import java.util.HashMap;
@@ -10,11 +12,15 @@ import javax.servlet.http.HttpServletRequest;
 
 public class B3HeadersService {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
+
     public void fillMDC(HttpServletRequest request, MDCAdapter mdc) {
+    	log.debug("MDC before fill {} ", mdc.getCopyOfContextMap());
         for (int i = 0; i < B3Header.values().length; i++) {
         	String b3Header = B3Header.values()[i].getName();
         	Optional.ofNullable(request.getHeader(b3Header)).ifPresent(h -> mdc.put(b3Header, h));
 		}
+        log.debug("MDC after fill {} ", mdc.getCopyOfContextMap());
     }
 
     public void cleanMDC(MDCAdapter mdc) {
@@ -30,6 +36,7 @@ public class B3HeadersService {
         	String b3Header = B3Header.values()[i].getName();
         	Optional.ofNullable(mdc.get(b3Header)).ifPresent(v -> headers.put(b3Header, v));
     	}
+    	log.debug("MDC headers map {} ", headers);
     	return headers;
     }
 }
